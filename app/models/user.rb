a4e45f3f0aad826e7409ebad21ce091e8487b66d
alex <alexmhorn@gmail.com>
2017-12-25 01:03:24 +0900

    added name to user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ApplicationRecord
   has_many :skill_tags, through: :skills
 
   has_many :endorsements
+
+  before_create :set_blank_name
+ 
+  private
+
+  def set_blank_name
+  	if self.name == nil
+	    self.name = ""
+	  end
+  end
 end
